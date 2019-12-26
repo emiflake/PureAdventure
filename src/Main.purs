1ba69f62483ade9fa943d29b0d9d460749eee710
Brandon Elam Barker <beb82@cornell.edu>
2019-12-26 18:45:18 +0000

    prototyping handle_command (not visible from JS, however)

--- src/Main.purs
+++ src/Main.purs
@@ -5,10 +5,16 @@ import Prelude
 import Adventure.Log (log)
 import Bot.Behavior (runBot)
 import Effect (Effect)
-import Effect.Aff (launchAff_)
+import Effect.Aff (Aff, launchAff_)
 
 main :: Effect Unit
 main =
   launchAff_ do
     log "Hello from PureScript"
+    _ <- handle_command "foo" ["bar", "baz"]
     runBot
+
+handle_command :: String -> Array String -> Aff Boolean
+handle_command cmd args = do
+  log $ (show cmd) <> (show args)
+  pure true
